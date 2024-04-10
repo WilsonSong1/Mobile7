4298657748564755496e960d49da2c69a166ac80
unknown <G00418330@atu.ie>
2024-04-10 10:33:42 +0000

    First commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle } from '@ionic/angular/standalone';
+import { MovieService } from '../Services/movie.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle],
+})
+export class HomePage implements OnInit{
+  movies:any=[];
+  constructor(private movieService:MovieService) {
+
+
+  }
+  ngOnInit(): void {
+    this.movieService.GetMovieData().subscribe(
+      (data)=>{
+        this.movies = data.Search;
+      }
+    );
+  }
+}
